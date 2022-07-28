14691e25af898f642c886b06eb868b6604b2f0f3
Ardyon <94380546+AidenRaaphorst@users.noreply.github.com>
2022-07-28 02:25:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Free Epic Games Discord Bot
+This Discord Bot will notify you when there are new free games in the Epic Games Store.\
+Heavily inspired by https://freestuffbot.xyz/ \
+\
+I made this because I didn't like that there was a delay that was hours long because of the amount of servers the FreeStuff bot had to ping.\
+Also because I wanted to start making my own Discord bots.
+
+## Usage
+Just run 'main.py', put in your bot token, token  will get saved in a .env file and it will search for free games!\
+\
+There are some variables you probably want to edit:\
+`iso_country_code = "NL"`: uses the correct country for the api, this will get the correct date and currency\
+`client = commands.Bot(command_prefix='!')`: you can edit the command prefix to something you like\
+`channel_name = "free-games-bot"`: specify in which channel the messages get sent to\
+`check_delay = 5`: the amount of minutes the bot will wait before trying to find new games again
---
+++ main.py
@@ -0,0 +1,148 @@
+import os
+import time
+import requests
+import json.decoder
+from dotenv import load_dotenv  # If not installed, run: pip install python-dotenv
+
+import discord  # If not installed, run: pip install discord
+from discord.ext import tasks
+from discord.ext import commands
+
+
+# User can edit these variables
+iso_country_code = "NL"  # Use your country code for the correct time and currency
+client = commands.Bot(command_prefix='!')
+channel_name = "free-games-bot"
+check_delay = 5  # Minutes
+
+if not os.path.exists('.env'):
+    BOT_TOKEN = input("Insert Discord Bot Token: ")
+    print()
+    with open('.env', 'w') as f:
+        f.write(f"TOKEN={BOT_TOKEN}")
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+old_games = []
+old_games_file_name = "old_games.json"
+
+
+@client.event
+async def on_ready():
+    print(f"{get_time()}: {client.user} has logged in")
+
+    try:
+        load_old_games()
+        print(f"{get_time()}: Loaded old embeds")
+    except FileNotFoundError as e:
+        print(f"{get_time()}: Error: \"{e}\"")
+        # print(f"{get_time()}: Error: Couldn't find & load file '{old_games_file_name}'")
+
+    await client.change_presence(
+        status=discord.Status.online,
+        activity=discord.Activity(
+            type=discord.ActivityType.listening,
+            name=f"{client.command_prefix}freegames"
+        )
+    )
+
+    check_and_send_embeds.start(discord.utils.get(client.get_all_channels(), name=channel_name))
+
+
+@tasks.loop(minutes=check_delay)
+async def check_and_send_embeds(channel: discord.TextChannel):
+    global old_games
+
+    games = get_free_games()
+
+    # No games found
+    if not games:
+        print(f"{get_time()}: No free games found in Epic Games Store")
+        return
+
+    # No new games found
+    if old_games:
+        if get_free_games()[0]['title'] == old_games[0]['title']:
+            print(f"{get_time()}: No new free games found")
+            return
+
+    # New games found
+    old_games = games
+    save_old_games()
+
+    print(f"{get_time()}: New free games found: ")
+    for embed in make_embeds(games):
+        print(embed.title.title())
+        await channel.send(embed=embed)
+
+
+@client.command()
+async def freegames(ctx: discord.Message):
+    embeds = make_embeds(get_free_games())
+
+    if not embeds:
+        await ctx.reply("No free games found", mention_author=False)
+        return
+
+    for embed in embeds:
+        await ctx.reply(embed=embed, mention_author=False)
+
+
+def make_embeds(games: list):
+    embeds = []
+    for game in games:
+        title = game['title']
+        description = game['description'].replace(". ", ".\n> ")
+        og_price = game['price']['totalPrice']['fmtPrice']['originalPrice']
+        end_date = game['promotions']['promotionalOffers'][0]['promotionalOffers'][0]['endDate']
+        image = game['keyImages'][3]['url']
+
+        embed = discord.Embed(
+            title=title,
+            description=f"> {description}\n\n"
+                        f"~~{og_price}~~ **Free** until {str(end_date).split('T')[0]}\n",
+            color=0x2f3136
+        )
+        embed.set_thumbnail(url="https://upload.wikimedia.org/wikipedia/commons/a/a7/Epic_Games_logo.png")
+        embed.set_image(url=image)
+        embed.set_footer(text="via Ardyon's Discord Bot")
+
+        embeds.append(embed)
+    return embeds
+
+
+def get_free_games():
+    free_games = []
+    epic_games_store_data = requests.get(
+        f"https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions?country={iso_country_code}"
+    ).json()
+    games = epic_games_store_data['data']['Catalog']['searchStore']['elements']
+    for game in games:
+        discount_price = game['price']['totalPrice']['discountPrice']
+        og_price = game['price']['totalPrice']['originalPrice']
+
+        if discount_price == 0 and og_price > 0:
+            free_games.append(game)
+
+    return free_games
+
+
+def save_old_games():
+    global old_games
+
+    with open(old_games_file_name, 'w') as f:
+        json.dump(old_games, f)
+
+
+def load_old_games():
+    global old_games
+
+    with open(old_games_file_name) as f:
+        old_games = json.load(f)
+
+
+def get_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+
+client.run(TOKEN)
